c987cba6cb66cad650c00b4e7c96959b7bceadb2
durveshramane26 <durveshramane6@gmail.com>
2022-07-18 20:15:02 +0530

    added output

---
+++ app.js
@@ -0,0 +1,59 @@
+const priceInputs =  document.querySelectorAll("input");
+const form = document.querySelector("#myform");
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    //collection prices
+
+    let purchasePrice = Number(priceInputs[0].value);
+    let stockQuantity = Number(priceInput[1].value);
+    let currentPrice = Number(priceInputs[2].value);
+
+    // console.log(purchasePrice, stockQuantity, currentPrice)
+
+    if(purchasePrice > 0 && stockQuantity > 0 && currentPrice > 0){
+        if(purchasePrice > currentPrice) {
+            //total loss in cash
+
+            const totalLoss = ((purchasePrice - currentPrice) * stockQuantity).toFixed(2);
+
+            const lossPerce = ((purchasePrice - currentPrice * 100)/ purchasePrice.toFixed(2));
+
+            if(lossPerce > 0) {
+                document.querySelector(".background").style.backgroundImage = "url('./gif2.webp')";
+                document.querySelector(".background").style.backgroundSize = "cover";
+                document.querySelector(".background").style.backgroundPosition = "center";
+            }
+
+            var losses = ""
+            losses = `<div class="loss-info>
+                        <p> You Lost ${lossPerce}%, and Your total loss is ₹${totalLoss}</p>
+                      </div>
+                    `;
+            document.querySelector(".output").innerHTML = losses;
+        }
+        else {
+            //total profit in cash
+            const totalprofit = ((currentPrice - purchasePrice) * stockQuantity).toFixed(2);
+            //profit in percentage
+            const profitPer = (((currentPrice - purchasePrice) * 100) / purchasePrice).toFixed(2);
+            //profitPercentage(profitPer)
+
+            if(profitPer > 0) {
+                document.querySelector(".background").style.backgroundImage = "url('./giphy.webp')";
+                document.querySelector(".background").style.backgroundSize = "cover";
+                document.querySelector(".background").style.backgroundPosition = "center";
+                
+            }
+
+            var prof="";
+            prof =`
+                    <div class="loss-info">
+                        <p>You gain ${profitPer}%, and Your total profit is ₹${totalprofit}</p>
+                    </div>
+                `;
+            document.querySelector(".output").innerHTML = prof;
+        }
+    }
+})
